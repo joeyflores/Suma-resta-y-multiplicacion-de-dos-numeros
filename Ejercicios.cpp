0c9b1c25bc471c5d2c47ebd2f17690b1b24cc4af
joeyflores <54363951+joeyflores@users.noreply.github.com>
2020-04-07 08:32:18 -0500

    Add files via upload

---
+++ Ejercicios.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+    int suma,resta,multi;
+
+    cout<<"Ingrese un valor para x: ";cin>>x;
+    cout<<"Ingrese un valor para y: ";cin>>y;
+
+    suma=x+y;
+    resta=x-y;
+    multi=x*y;
+    cout<<"La suma es: "<<suma<<endl;
+    cout<<"La resta es: "<<resta<<endl;
+    cout<<"La multiplicacion es: "<<multi<<endl;
+
+
+    return 0;
+}
